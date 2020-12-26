051f76011627ce93a05167a186533ae0a3d0c590
Pierre-Yves Flamand <46058408+PierreYvesFlamand@users.noreply.github.com>
2020-12-26 20:07:26 +0100

    Correct Projects ratio

--- src/containers/Projects/Projects.js
+++ src/containers/Projects/Projects.js
@@ -6,18 +6,18 @@ export default function Projects() {
     return (
         <section className='projects' id='my-projects'>
             <div className='projects-container'>
-                <h2>Projetcs</h2>
+                <h2>Projects</h2>
                 <div className='all-projetcs'>
                     <div className='project'>
                         <div className='img' style={{ backgroundImage: 'url(./data/img/avatar.png)' }}></div>
-                        <div className='hover'>
-                            <a
-                                href='#'
-                                onClick={() => {
-                                    document.querySelector('.project-modal').classList.add('show');
-                                    document.body.style.overflow = 'hidden';
-                                }}
-                            >
+                        <div
+                            className='hover'
+                            onClick={() => {
+                                document.querySelector('.project-modal').classList.add('show');
+                                document.body.style.overflow = 'hidden';
+                            }}
+                        >
+                            <a href='#'>
                                 <i className='far fa-eye'></i>VIEW PROJECT
                             </a>
                         </div>
