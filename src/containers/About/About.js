3ae2d493b6c4f9d5a6a5098b1bc210ba1052b7ed
Pierre-Yves Flamand <46058408+PierreYvesFlamand@users.noreply.github.com>
2020-12-29 16:58:46 +0100

    Finished About

--- src/containers/About/About.js
+++ src/containers/About/About.js
@@ -24,15 +24,34 @@ export default function About(props) {
         <section className='about' id='a-propos'>
             <div className={`about-container${fadeIn ? ' fade-in' : ''}`} ref={ref}>
                 <h2>A propos</h2>
-                {text.map((text, idx) => (
-                    <p key={idx}>{text}</p>
-                ))}
-                <p className='cv-link'>
-                    <a href='CV_Pierre-Yves_Flamand.pdf' download>
-                        Télécharger mon cv (53.8Ko)
-                    </a>
-                    <i className='fas fa-download'></i>
-                </p>
+
+                <div className='about-tech'>
+                    <i className='fab fa-html5 html'></i>
+                    <i className='fab fa-css3-alt css'></i>
+                    <i className='fab fa-js js'></i>
+                    <i className='fab fa-react react'></i>
+                    <i className='fab fa-sass sass'></i>
+                    <i className='fab fa-git-alt git'></i>
+                    <i className='fab fa-gulp gulp'></i>
+                    <img src='./data/img/webpack.png' alt='' className='webpack' />
+                </div>
+
+                <div className='about-content'>
+                    {text.map((text, idx) => (
+                        <p key={idx}>{text}</p>
+                    ))}
+                    <p className='cv-link'>
+                        <a href='CV_Pierre-Yves_Flamand.pdf' download>
+                            Télécharger mon cv (53.8Ko)
+                        </a>
+                        <i className='fas fa-download'></i>
+                    </p>
+                </div>
+
+                <a className='header-cta' href='#mes-projets'>
+                    <span className='hover'>mes projets</span>
+                    <span className='link'>mes projets</span>
+                </a>
             </div>
         </section>
     );
