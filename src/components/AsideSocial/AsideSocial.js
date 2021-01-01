332b2cf823bf3839f0db9c52df23938cb553444a
Pierre-Yves Flamand <46058408+PierreYvesFlamand@users.noreply.github.com>
2021-01-01 12:43:15 +0100

    Added Data from AsideSocial

--- src/components/AsideSocial/AsideSocial.js
+++ src/components/AsideSocial/AsideSocial.js
@@ -1,31 +1,35 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import './AsideSocial.css';
 
+import { DataContext } from '../../context/dataContext';
+
 export default function AsideSocial() {
+    const data = useContext(DataContext).data.asideSocial;
+
     return (
         <aside className='aside-social'>
             <ul>
                 <li className='linkedin'>
-                    <a href='https://www.linkedin.com/in/pierreyvesflamand/' target='_blank' rel='noreferrer'>
-                        <span>Linkedin</span>
+                    <a href={data[0].link} target='_blank' rel='noreferrer'>
+                        <span>{data[0].text}</span>
                         <i className='fab fa-linkedin'></i>
                     </a>
                 </li>
                 <li className='github'>
-                    <a href='https://github.com/pierreyvesflamand' target='_blank' rel='noreferrer'>
-                        <span>Github</span>
+                    <a href={data[1].link} target='_blank' rel='noreferrer'>
+                        <span>{data[1].text}</span>
                         <i className='fab fa-github'></i>
                     </a>
                 </li>
                 <li className='email'>
-                    <a href='mailto:contact@pierreyvesflamand.com'>
-                        <span>Email</span>
+                    <a href={data[2].link}>
+                        <span>{data[2].text}</span>
                         <i className='far fa-envelope'></i>
                     </a>
                 </li>
                 <li className='resume'>
-                    <a href='CV_Pierre-Yves_Flamand.pdf' target='_blank' rel='noreferrer'>
-                        <span>Mon CV</span>
+                    <a href={data[3].link} target='_blank' rel='noreferrer'>
+                        <span>{data[3].text}</span>
                         <i className='fas fa-file'></i>
                     </a>
                 </li>
