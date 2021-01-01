b0224495ce3029072f3129dfb3a3eb64b0ab093e
Pierre-Yves Flamand <46058408+PierreYvesFlamand@users.noreply.github.com>
2021-01-01 12:28:00 +0100

    Refactoring AsideSocial

--- src/components/AsideSocial/AsideSocial.js
+++ src/components/AsideSocial/AsideSocial.js
@@ -5,25 +5,25 @@ export default function AsideSocial() {
     return (
         <aside className='aside-social'>
             <ul>
-                <li className='aside-social-linkedin'>
+                <li className='linkedin'>
                     <a href='https://www.linkedin.com/in/pierreyvesflamand/' target='_blank' rel='noreferrer'>
                         <span>Linkedin</span>
                         <i className='fab fa-linkedin'></i>
                     </a>
                 </li>
-                <li className='aside-social-github'>
+                <li className='github'>
                     <a href='https://github.com/pierreyvesflamand' target='_blank' rel='noreferrer'>
                         <span>Github</span>
                         <i className='fab fa-github'></i>
                     </a>
                 </li>
-                <li className='aside-social-email'>
+                <li className='email'>
                     <a href='mailto:contact@pierreyvesflamand.com'>
                         <span>Email</span>
                         <i className='far fa-envelope'></i>
                     </a>
                 </li>
-                <li className='aside-social-resume'>
+                <li className='resume'>
                     <a href='CV_Pierre-Yves_Flamand.pdf' target='_blank' rel='noreferrer'>
                         <span>Mon CV</span>
                         <i className='fas fa-file'></i>
